daea24637d008a7d10ff99d2639a2780ca59b3e2
kissibaah <paaquesi143@gmail.com>
2024-03-29 05:56:47 +0000

    Python Network ALX project

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This python script sends a search request
+    for a given string to the Star Wars API.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people"
+    params = {"search": sys.argv[1]}
+    results = requests.get(url, params=params).json()
+
+    print("Number of results: {}".format(results.get("count")))
+    [print(r.get("name")) for r in results.get("results")]
